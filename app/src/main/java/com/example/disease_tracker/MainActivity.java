fa8f071009db929742de0a3b6879020cd706b3f4
Nhlakanipho02 <nhlakaniphomdluli2@gmail.com>
2020-06-13 07:55:02 +0200

    project

--- app/src/main/java/com/example/disease_tracker/MainActivity.java
+++ app/src/main/java/com/example/disease_tracker/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.disease_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button Log_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log_in = findViewById(R.id.Log_In);
+         Log_in.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent (MainActivity.this , MainLogin.class);
+                 startActivity(intent);
+             }
+         });
+
+        };
     }
-}
