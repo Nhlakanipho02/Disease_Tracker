3e8a2d9641cf41eb8494fc6ed59373f526e10bf2
Nhlakanipho02 <nhlakaniphomdluli2@gmail.com>
2020-06-13 15:26:50 +0200

    project

--- app/src/main/java/com/example/disease_tracker/MainActivity.java
+++ app/src/main/java/com/example/disease_tracker/MainActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button Log_in;
+    public TextView Register;
+    public Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log_in = findViewById(R.id.Log_In);
-         Log_in.setOnClickListener(new View.OnClickListener() {
+        Log_in.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 Intent intent = new Intent (MainActivity.this , MainLogin.class);
+                 intent = new Intent (MainActivity.this , MainLogin.class);
                  startActivity(intent);
              }
          });
+
+        Register = findViewById(R.id.Register);
+        Register.setOnClickListener(v -> {
+            intent = new Intent (MainActivity.this , Register.class);
+            startActivity(intent);
+        });
 
         };
     }
--- app/src/main/java/com/example/disease_tracker/MainLogin.java
+++ app/src/main/java/com/example/disease_tracker/MainLogin.java
@@ -2,13 +2,41 @@ package com.example.disease_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainLogin extends AppCompatActivity {
+
+    public Button Log_in;
+    public TextView Register;
+    public Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Log_in = findViewById(R.id.LOGIN);
+        Log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainLogin.this , Home.class);
+                startActivity(intent);
+            }
+        });
+
+        Register = findViewById(R.id.Register);
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainLogin.this , Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
